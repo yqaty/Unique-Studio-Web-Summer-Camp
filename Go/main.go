c607cbfd5d8a502d58429ad046ae418b4a6a597e
esc <2024588807@qq.com>
2023-07-16 18:44:03 +0800

    formatted

--- Go/main.go
+++ Go/main.go
@@ -4,38 +4,40 @@ import (
 	"context"
 	"fmt"
 	"github.com/chromedp/chromedp"
-	"log"
-	"time"
-	"strings"
-	"strconv"
-	"os"
-	"net/http"
-	"path/filepath"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"sync"
+	"time"
 )
 
 const (
 	PreUserUrl = "https://www.vilipix.com/user/"
-	PreArtUrl = "https://www.vilipix.com/illust/"
+	PreArtUrl  = "https://www.vilipix.com/illust/"
 )
 
 func IndexAll(s, t string) (index []int) {
-    for pos := strings.Index(s, t); pos != -1; {
-        index = append(index, pos)
-        s = s[pos+1:]
-        pos = strings.Index(s,t)
-    }
+	for pos := strings.Index(s, t); pos != -1; {
+		index = append(index, pos)
+		s = s[pos+1:]
+		pos = strings.Index(s, t)
+	}
 	for i := 1; i < len(index); i++ {
 		index[i] += index[i-1] + 1
 	}
-    return
+	return
 }
 
 func GetPicId(s string) string {
 	var st, ed int
-	for st = 0; st < len(s) && (s[st] < '0' || s[st] > '9'); st++ {}
-	for ed = st; ed < len(s) && (s[ed] >= '0' && s[ed] <= '9'); ed++ {}
+	for st = 0; st < len(s) && (s[st] < '0' || s[st] > '9'); st++ {
+	}
+	for ed = st; ed < len(s) && (s[ed] >= '0' && s[ed] <= '9'); ed++ {
+	}
 	return s[st:ed]
 }
 
@@ -72,34 +74,34 @@ func GetPicIds(url string) map[string]bool {
 	selector := "body > div"
 	param := `document.querySelector("body")`
 	var picid = make(map[string]bool)
-	for i := 1 ;; i++ {
-		html, _ := GetHttpHtmlContent(url + "?p=" + strconv.Itoa(i), selector, param)
+	for i := 1; ; i++ {
+		html, _ := GetHttpHtmlContent(url+"?p="+strconv.Itoa(i), selector, param)
 		s := string(html)
 		const t = `href="/illust`
 		index := IndexAll(s, t)
 		if len(index) == 0 {
 			break
 		}
-		fmt.Printf("Fetch %d images on page %d!\n",len(index)/2,i)
-		for _,pos := range index {
+		fmt.Printf("Fetch %d images on page %d!\n", len(index)/2, i)
+		for _, pos := range index {
 			picid[GetPicId(s[pos:pos+30])] = true
 		}
 	}
 	return picid
 }
 
-func Gethtml(url string) []uint8{
+func Gethtml(url string) []uint8 {
 	resp, err := http.Get(url)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Gethtml: %v\n", err)
-        return nil
-     }
-    htm, err := ioutil.ReadAll(resp.Body)
-    resp.Body.Close()
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Gethtml: reading %s: %v\n", url, err)
-        return nil
-    }
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Gethtml: %v\n", err)
+		return nil
+	}
+	htm, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Gethtml: reading %s: %v\n", url, err)
+		return nil
+	}
 	return htm
 }
 
@@ -108,30 +110,30 @@ var tokens = make(chan struct{}, 20)
 func down(id string, wg *sync.WaitGroup, dir string) {
 	defer wg.Done()
 	tokens <- struct{}{}
-	fmt.Printf("Start downloading %s.jpg!\n",id)
+	fmt.Printf("Start downloading %s.jpg!\n", id)
 	url := PreArtUrl + id
 	s := string(Gethtml(url))
 	st := strings.Index(s, "https://img9.vilipix.com")
-	ed := st + 1 + strings.Index(s[st + 1:], "jpg")
-	url = s[st:ed+3]
-	file, err := os.Create(filepath.Join(".",dir,id + ".jpg"))
+	ed := st + 1 + strings.Index(s[st+1:], "jpg")
+	url = s[st : ed+3]
+	file, err := os.Create(filepath.Join(".", dir, id+".jpg"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "down create:%v\n", err)
 		return
 	}
 	defer file.Close()
 	file.Write([]byte(Gethtml(url)))
-	fmt.Printf("%s.jpg is downloaded now!\n",id)
+	fmt.Printf("%s.jpg is downloaded now!\n", id)
 	<-tokens
 }
 
 func download(picid map[string]bool, userid string) {
-	fmt.Printf("There are %d pictures in total!\nStart downloading now!\n",len(picid))
-	os.MkdirAll(userid + "artwork", 0755)
+	fmt.Printf("There are %d pictures in total!\nStart downloading now!\n", len(picid))
+	os.MkdirAll(userid+"artwork", 0755)
 	var wg sync.WaitGroup
 	for id := range picid {
 		wg.Add(1)
-		go down(id, &wg, userid + "artwork")
+		go down(id, &wg, userid+"artwork")
 	}
 	wg.Wait()
 }
@@ -142,5 +144,5 @@ func main() {
 	fmt.Scanf("%s", &userid)
 	url := PreUserUrl + userid
 	download(GetPicIds(url), userid)
-	return 
+	return
 }
